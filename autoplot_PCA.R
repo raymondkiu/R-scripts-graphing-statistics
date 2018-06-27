2851efda85bca48f4ac9cf6e1527277a1a49a9d9
Raymond Kiu <raymond.kiu@quadram.ac.uk>
2018-06-27 17:55:23 +0100

    quantile method

--- autoplot_PCA.R
+++ autoplot_PCA.R
@@ -11,6 +11,16 @@ head(data[1])
 df <- data[c(2:ncol(data))]
 head(df)
 
+########################################################################################################
+## Use 'quantile' method - ignore 0 in the matrix to get better distance clustering based on ranking  ##
+########################################################################################################
+#quants <- normalize.quantiles(as.matrix(df), copy = F)
+#pca.data <-prcomp(quants,
+#                  center = TRUE,
+#                  scale. = TRUE)
+# autoplot(pac.data, data = data, colour = 'Group', size =4, loadings.label = TRUE, label.size ="10", loadings.label.colour="black")
+
+
 png("PCA_test.png",
     width = 6*800,        # 5 x 300 pixels
     height = 6*800,
@@ -22,9 +32,11 @@ theme(legend.position="bottom", legend.direction="horizontal", legend.title = el
 scale_color_manual(values=c("BELL2/3"="Red","BELL1"="#8e44ad","Red","Non-NEC"="#28b463","Purple","Brown","Orange","Black"))+ 
    geom_point(aes(shape=Group, color=Group))+
   scale_shape_manual(values=c(3, 16, 17))
+
 #autoplot(pam(df, 2), frame = TRUE, frame.type = 'norm') 
 #autoplot(fanny(df, 3), frame = TRUE)+
 #  theme(legend.position="bottom", legend.direction="horizontal", legend.title = element_blank())
 #autoplot
+
 dev.off()
 
