bb91fd42496b905c4e096cfd02a9a9ce56b579f9
Raymond Kiu <raymond.kiu@quadram.ac.uk>
2018-10-03 13:18:50 +0100

    Create worldmap.R

---
+++ worldmap.R
@@ -0,0 +1,12 @@
+## Author: Raymond Kiu Raymond.Kiu@quadram.ac.uk
+
+#############################
+## create a world map in R ##
+#############################
+
+library(rworldmap)
+newmap <- getMap(resolution = "low", projection = NA) # resolution can be low, less islands, li or high
+plot(newmap)
+
+# create a European map
+# plot(newmap, xlim = c(-20, 59), ylim = c(35, 71), asp = 1)
