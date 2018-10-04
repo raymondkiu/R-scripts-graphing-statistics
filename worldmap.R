ccfd79bff7cc6ce8ccb7180ee1e9a1c80b2463f8
Raymond Kiu <raymond.kiu@quadram.ac.uk>
2018-10-04 13:12:36 +0100

    UK and Europe map

--- worldmap.R
+++ worldmap.R
@@ -10,3 +10,5 @@ plot(newmap)
 
 # create a European map
 # plot(newmap, xlim = c(-20, 59), ylim = c(35, 71), asp = 1, col = "lightgrey")
+#plot(newmap, xlim = c(-20, 59), ylim = c(35, 71), asp = 1, col = "gray88") # Europe
+#plot(newmap, xlim = c(-10, 0), ylim = c(45, 71), asp = 1, col = "gray88") # UK and Ireland
