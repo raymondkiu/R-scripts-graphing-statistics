b932963826657550d445d8f56301c635a610201c
Raymond Kiu <raymond.kiu@quadram.ac.uk>
2018-10-04 12:47:06 +0100

    add colour option

--- worldmap.R
+++ worldmap.R
@@ -9,4 +9,4 @@ newmap <- getMap(resolution = "low", projection = NA) # resolution can be low, l
 plot(newmap)
 
 # create a European map
-# plot(newmap, xlim = c(-20, 59), ylim = c(35, 71), asp = 1)
+# plot(newmap, xlim = c(-20, 59), ylim = c(35, 71), asp = 1, col = "lightgrey")
