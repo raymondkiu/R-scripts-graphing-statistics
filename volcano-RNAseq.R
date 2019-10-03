858b51afebacd538d5413f48c00fbbb7e94b0282
Raymond Kiu <raymond.kiu@quadram.ac.uk>
2019-10-03 14:59:03 +0100

    Update volcano-RNAseq.R

--- volcano-RNAseq.R
+++ volcano-RNAseq.R
@@ -1,10 +1,10 @@
 ## Volcano plot with "significant" genes labeled by labelsig=TRUE
-res <- read.csv("DE1.csv")
-
+res <- read.csv("DE.csv")
 
 volcanoplot <- function (res, lfcthresh=2, sigthresh=0.01, main="Volcano Plot", legendpos="bottomright", labelsig=FALSE, textcx=1, ...) {
   with(res, plot(log2FoldChange, -log10(pvalue), pch=20, main=main, ...))
   with(subset(res, padj<sigthresh ), points(log2FoldChange, -log10(pvalue), pch=20, col="red", ...))
+  with(subset(res, abs(log2FoldChange)<lfcthresh), points(log2FoldChange, -log10(pvalue), pch=20, col="grey", ...))
   with(subset(res, abs(log2FoldChange)>lfcthresh), points(log2FoldChange, -log10(pvalue), pch=20, col="grey", ...))
   with(subset(res, padj<sigthresh & (log2FoldChange)>lfcthresh), points(log2FoldChange, -log10(pvalue), pch=20, col="red", ...))
   with(subset(res, padj<sigthresh & (log2FoldChange)<lfcthresh), points(log2FoldChange, -log10(pvalue), pch=20, col="blue", ...))
@@ -12,8 +12,8 @@ volcanoplot <- function (res, lfcthresh=2, sigthresh=0.01, main="Volcano Plot", 
     require(calibrate)
     with(subset(res, padj<sigthresh & abs(log2FoldChange)>lfcthresh), textxy(log2FoldChange, -log10(pvalue), labs=Gene, cex=textcx, ...))
   }
-  legend(legendpos, xjust=0.6, yjust=0.6, legend=c(paste("down-regulated"), paste("not significant"), "up-regulated"), pch=15, col=c("blue","grey","red"))
+  legend(legendpos, xjust=0.4, yjust=0.4, legend=c(paste("down-regulated"), paste("not significant"), "up-regulated"), pch=20, col=c("blue","grey","red"))
 }
 png("diffexpr-volcanoplot.png", 1500, 1500, pointsize=40)
-volcanoplot(res, lfcthresh=0, sigthresh=0.01, textcx=.8, xlim=c(-12, 12))
+volcanoplot(res, lfcthresh=2, sigthresh=0.01, textcx=.8, xlim=c(-12, 12))
 dev.off()
