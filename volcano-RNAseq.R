b4fe8f980ddde1ddf402cba7747c522998539b35
Raymond Kiu <raymond.kiu@quadram.ac.uk>
2019-10-03 15:07:05 +0100

    Added format for CSV input

--- volcano-RNAseq.R
+++ volcano-RNAseq.R
@@ -1,6 +1,15 @@
 ## Volcano plot with "significant" genes labeled by labelsig=TRUE
 res <- read.csv("DE.csv")
 
+# format of csv
+# Gene,log2FoldChange,pvalue,padj
+# Gm48415,10.14034929,5.41E-16,1.15E-11
+# Smg1,9.604422597,3.00E-15,3.19E-11
+# Plec,8.422424515,9.91E-15,5.57E-11
+# Gm47578,9.230186615,1.05E-14,5.57E-11
+# Gm15500,-10.04933593,1.65E-14,7.02E-11
+# Gcc2,8.995148614,2.55E-14,8.34E-11
+
 volcanoplot <- function (res, lfcthresh=2, sigthresh=0.01, main="Volcano Plot", legendpos="bottomright", labelsig=FALSE, textcx=1, ...) {
   with(res, plot(log2FoldChange, -log10(pvalue), pch=20, main=main, ...))
   with(subset(res, padj<sigthresh ), points(log2FoldChange, -log10(pvalue), pch=20, col="red", ...))
