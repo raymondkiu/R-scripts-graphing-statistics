6045c6a64423ee117d306d50f416b24f8a9540dc
Raymond Kiu <raymond.kiu@quadram.ac.uk>
2019-10-03 15:03:57 +0100

    Added format of CSV file

--- MAplot.R
+++ MAplot.R
@@ -1,5 +1,14 @@
 res <- read.csv("DE2MA.csv")
 
+# CSV format as follows:
+# Gene,AveExpr,log2FoldChange,pvalue,padj
+# Gm48415,1.242150603,10.14034929,5.41E-16,1.15E-11
+# Smg1,0.972791678,9.604422597,3.00E-15,3.19E-11
+# Plec,0.383659158,8.422424515,9.91E-15,5.57E-11
+# Gm47578,0.787138905,9.230186615,1.05E-14,5.57E-11
+# Gm15500,1.471363151,-10.04933593,1.65E-14,7.02E-11
+
+
 maplot <- function (res, thresh=0.01, labelsig=FALSE, textcx=1, ...) {
   with(res, plot(AveExpr, log2FoldChange, pch=20, col="grey", cex=1, ...))
   with(subset(res, padj<thresh), points(AveExpr, log2FoldChange, col="red", pch=20, cex=1))
