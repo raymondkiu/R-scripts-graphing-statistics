74b96bd7cbb09a8ba2ec84d4f52ba5a23d5c01bf
Raymond Kiu <raymond.kiu@quadram.ac.uk>
2022-11-15 15:50:03 +0000

    Simplified and updated

--- rarecurve.R
+++ rarecurve.R
@@ -1,29 +1,20 @@
-library(Hotelling)
 library(vegan)
-library(plyr)
 
-#####colorsfor rarecurve
-col <- c("darkgreen", "green3","green","red4", "red2", "red", "navy",  "royalblue","skyblue1","purple4","violet","cyan3","green","navy","darkgreen","pink")
+setwd("Desktop/")
+
+#colorsfor rarecurve
+col <- c("NE1"="darkgreen","NE2"="pink","NE3"="purple")
+
+#line
 #lty <- c("solid", "dashed", "longdash", "dotdash")
 
-X <- read.csv("Genus-only.csv",row.names=1)
+# input format in csv (read counts, the file should be csv)
+#Sample	Moraxella osloensis	Corynebacterium striatum	Pseudomonas oryzihabitans	Staphylococcus epidermidis	Pseudomonas resinovorans	Micrococcus luteus	Streptococcus mitis	Streptococcus pneumoniae	Cutibacterium acnes
+#NE1	100	689	0	45	0	3112	1234	12333	122
+#NE2	200	10300	16900	12	1	443	555	1234	11
+#NE3	100	1700	11224	3333	10	3455	800	12222	11
+
+X <- read.csv("test.csv",row.names=1)
 head(X)
-Y <- t(X)
-pdf("rarecurve_2.pdf",6,6)
 
-rarecurve(Y, step = 100, xlab= "No. of normalised reads", ylab = "No. of genera",lwd=4,col = col,lty = "solid", label = FALSE)
-legend("bottomright", 
-       legend = c(row.names(Y)), 
-       col = col, 
-       pch =26:31, 
-       lwd=4,
-       ncol=2,
-       x.intersp=0.5,
-       bty = "n", 
-       pt.cex = 5, 
-       cex = 0.8, 
-       text.col = "black", 
-       horiz = F , 
-       inset = c(0.1, 0.1, 0.1))
-
-dev.off()
+rarecurve(X, step = 100, xlab= "No. of reads", ylab = "No. of genera",lwd=4,col=col,lty = "solid", label = TRUE)
