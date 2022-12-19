c1d943bfad49a83fc57a9c57135f5f7d223b08a7
Raymond Kiu <raymond.kiu@quadram.ac.uk>
2022-12-19 10:46:28 +0000

    Create Melonnpan.R
    Incomplete

---
+++ Melonnpan.R
@@ -0,0 +1,5 @@
+install.packages('devtools') # Install devtools if not installed already
+library(devtools) # Load devtools
+devtools::install_github("biobakery/melonnpan") # Install MelonnPan
+
+# it uses output of humann2
